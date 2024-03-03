a242d511038e99e822f40ba00950bf9e59b42cdb
banziFD <supengyumail@gmail.com>
2024-03-03 09:46:17 +0800

    promise-allsettled

--- nodejs/main.ts
+++ nodejs/main.ts
@@ -1,10 +1,9 @@
-import { publicMain } from "./src/public-api/public-api";
+import { promiseAllSettledMain } from "./src/language-feature/promise-allsettled";
 
 const main = async (): Promise<void> => {
   console.log("start");
   const start = new Date().getTime();
-  const res = await publicMain();
-  console.log(JSON.stringify(res.data));
+  const res = await promiseAllSettledMain();
   console.log((new Date().getTime() - start) / 1000);
 };
 
